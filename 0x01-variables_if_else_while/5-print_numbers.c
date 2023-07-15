1918f2651d9c4ce3cad1a6d50b0eb9ae40e057d0
tokamagdy <tokamagdy545@gmail.com>
2023-07-14 17:02:17 -0700

    ${ver}

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-int digit = 0;
-while (digit <= 9)
-{
-printf("%i".digit);
-digit++;
-}
-printf("\n");
-return (0);
+	int digit = 0;
+	while (digit <= 9)
+	{
+		printf("%i".digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
 }
