4e2754cda483691b6ed9787892ad3adc3a699246
tokamagdy <tokamagdy545@gmail.com>
2023-07-14 19:32:39 -0700

    ${ver}

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,17 @@
 */
 int main(void)
 {
-int digit = 0;
-while (digit <= 9)
+int n;
+for (n = 48; n <= 57; n++)
 {
-putchar(digit + 48);
-if (digit != 9)
+putchar(n);
+if (n == 57)
 {
+continue;
+}
 putchar(',');
 putchar(' ');
 }
-putchar('\n')
+putchar('\n');
 return (0);
 }
