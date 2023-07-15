421b0305ffbf17cd532ded1d03b63e1dbfebf9a0
tokamagdy <tokamagdy545@gmail.com>
2023-07-14 18:03:58 -0700

    ${ver}

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,21 +2,20 @@
 
 /**
 * main - Entry Point
-* Description: print number of base 16
+* Description: print number of base16 in lowercase
 * Return: Always 0 (success)
 */
 int main(void)
 {
-int n;
-int m;
-for (n = 48; n <= 57; n++)
+int digit = 48; /*48; decimal rep of 0*/
+while (digit <= 102) /*102; decimal rep of f*/
 {
-putchar(n);
+putchar(digit);
+/* after 9 we jump till 96: */
+if (digit == 57)
+digit += 39;
+digit++;
 }
-for (m = 97; m <= 102; m++)
-{
-putchar(m);
-}
-putchar("\n");
+putchar('\n');
 return (0);
 }
