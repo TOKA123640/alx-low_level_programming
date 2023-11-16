3fa6d899d50c3dc3f9f9a380f33685fe7a697b74
toka magdy <tokamagdy545@gmail.com>
2023-11-16 14:00:06 -0800

    print

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LINKED
-#define LINKED
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <string.h>
@@ -15,9 +15,9 @@
 */
 typedef struct list_s
 {
-char *str:
-unsigned int len;
-struct list_s *next;
+	char *str:
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -26,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /*LINKED/
+#endif /*LISTS_H*/
